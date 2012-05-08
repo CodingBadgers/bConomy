b2185c16db3d43ffe21082da13cca33154139209
n3wton <n3wton@59879121-d2b3-4da0-87af-382deb114e0d>
2012-05-08 07:24:28 +0000

    Started adding commands
    git-svn-id: https://subversion.assembla.com/svn/bconomy@19 59879121-d2b3-4da0-87af-382deb114e0d

--- src/uk/badger/bConomy/bConomy.java
+++ src/uk/badger/bConomy/bConomy.java
@@ -1,5 +1,7 @@
 package uk.badger.bConomy;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import uk.badger.bConomy.config.Config;
@@ -15,6 +17,35 @@ public class bConomy extends JavaPlugin {
 		// setup config and database
 		Config.setupConfig();
 		DatabaseManager.setupDatabase(this);
+	}
+	
+	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
+		
+		if (commandLabel.equalsIgnoreCase("money")) {
+			
+			// show the help
+			if (args[0].equalsIgnoreCase("?") || args[0].equalsIgnoreCase("help")) {
+				showHelp(sender);
+				return true;
+			}
+			
+			return true;
+		}
+		
+		return false;
+	}
+
+	private void showHelp(CommandSender sender) {
+		sender.sendMessage("-- bConomy --");
+		
+		sender.sendMessage("/money [name] - Displays the amount of money in an account");
+		sender.sendMessage("/money pay (name) (amount) - Pays a player");
+		sender.sendMessage("/money top [amount] - Shows the top player balances");
+		
+		sender.sendMessage("/money grant (name) (amount) - Grants a player money");
+		sender.sendMessage("/money withdraw (name) (amount) - Withdraws money from a player");
+		sender.sendMessage("/money set (name) (amount) - Sets a players balance");
+		sender.sendMessage("/money reset (name) - Resets a players balance");
 	}
 	
 }
